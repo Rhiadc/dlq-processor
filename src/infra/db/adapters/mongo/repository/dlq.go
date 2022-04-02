f141110fa68accd537e394199068a6ae939e2541
Rhiad <rhiad.ciccoli@gmail.com>
2022-04-02 20:09:14 -0300

    update dlq repository

--- src/infra/db/adapters/mongo/repository/dlq.go
+++ src/infra/db/adapters/mongo/repository/dlq.go
@@ -1,20 +1,117 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
 type DLQRecord struct {
-	//@TODO
-	//date
-	//msg
-	//processed
+	Date      time.Time `bson:"date"`
+	Msg       string    `bson:"message"`
+	Processed bool      `bson:"processed"`
 }
 
 type DLQRepository struct {
+	database *mongo.Database
+}
+type DLQRepo interface {
 	//@TODO
-	//get all DLQRecords
-	//get DLQRecord by date
-	//get all DLQRecords from date
-	//get all processed DLQRecords
-	//get unprocessed DLQRecords
-	//create DLQRecord
 	//delete DLQRecord
 	//update DLQRecord
+}
+
+var (
+	DLQCollection = "dlq"
+)
+
+func NewDLQRepository(database *mongo.Database) *DLQRepository {
+	return &DLQRepository{database: database}
+}
+
+func (ref DLQRepository) InsertMessage(m DLQRecord) error {
+	coll := ref.database.Collection(DLQCollection)
+	_, err := coll.InsertOne(context.TODO(), m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ref DLQRepository) GetAllMessages() ([]DLQRecord, error) {
+	var msg DLQRecord
+	var DLQRecords []DLQRecord
+	coll := ref.database.Collection(DLQCollection)
+	cursor, err := coll.Find(context.TODO(), bson.D{})
+	if err != nil {
+		defer cursor.Close(context.TODO())
+		return nil, err
+	}
+
+	for cursor.Next(context.TODO()) {
+		err := cursor.Decode(&msg)
+		if err != nil {
+			return DLQRecords, err
+		}
+		DLQRecords = append(DLQRecords, msg)
+	}
+	return DLQRecords, nil
+}
+
+func (ref DLQRepository) GetMessagesByDateRange(days int) ([]DLQRecord, error) {
+	var msg DLQRecord
+	var DLQRecords []DLQRecord
+	coll := ref.database.Collection(DLQCollection)
+	cursor, err := coll.Find(context.TODO(), bson.M{"date": bson.M{
+		"$gte": primitive.NewDateTimeFromTime(time.Now().AddDate(0, 0, days)),
+	}})
+	if err != nil {
+		defer cursor.Close(context.TODO())
+		return nil, err
+	}
+
+	for cursor.Next(context.TODO()) {
+		err := cursor.Decode(&msg)
+		if err != nil {
+			return DLQRecords, err
+		}
+		DLQRecords = append(DLQRecords, msg)
+	}
+	return DLQRecords, nil
+}
+
+func (ref DLQRepository) GetProcessedMessages() ([]DLQRecord, error) {
+	var msg DLQRecord
+	var DLQRecords []DLQRecord
+	coll := ref.database.Collection(DLQCollection)
+	cursor, err := coll.Find(context.TODO(), bson.M{
+		"processed": bson.M{"$ne": false},
+	})
+	if err != nil {
+		defer cursor.Close(context.TODO())
+		return nil, err
+	}
+
+	for cursor.Next(context.TODO()) {
+		err := cursor.Decode(&msg)
+		if err != nil {
+			return DLQRecords, err
+		}
+		DLQRecords = append(DLQRecords, msg)
+
+	}
+	fmt.Println(DLQRecords)
+	return DLQRecords, nil
+}
+
+func (ref DLQRepository) DeleteMessageByDate(date time.Time) error {
+	coll := ref.database.Collection(DLQCollection)
+	_, err := coll.DeleteOne(context.TODO(), bson.D{{"date", primitive.NewDateTimeFromTime(date)}})
+	if err != nil {
+		return err
+	}
+	return nil
 }
